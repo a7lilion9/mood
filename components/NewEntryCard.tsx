700b3b35b796773ca0b613b77a2480d76da1c314
Ahmed Hlilioune <a7lilion3@gmail.com>
2024-05-28 21:46:32 +0100

    Section 3 completed

---
+++ components/NewEntryCard.tsx
@@ -0,0 +1,11 @@
+const NewEntryCard = () => {
+  return (
+    <div className="cursor-pointer divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+      <div className="px-4 py-5 sm:p-6">
+        <span className="text-xl">New entry</span>
+      </div>
+    </div>
+  );
+};
+
+export default NewEntryCard;
