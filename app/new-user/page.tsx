66496d34780ccf249d905c6dfc66607e8fb64a49
Ahmed Hlilioune <a7lilion3@gmail.com>
2024-05-28 14:47:32 +0100

    Section 3 - In progress

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,33 @@
-const NewUser = () => {
-  return <div>Hi</div>;
+import { prisma } from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { userAgent } from "next/server";
+
+const createNewUser = async () => {
+  const user = await currentUser();
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id as string,
+    },
+  });
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
+      },
+    });
+  }
+
+  redirect("/journal");
+};
+
+const NewUser = async () => {
+  await createNewUser();
+
+  return <div>...loading</div>;
 };
 
 export default NewUser;
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/journal"]);
+const isProtectedRoute = createRouteMatcher(["/journal", "/new-user"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
